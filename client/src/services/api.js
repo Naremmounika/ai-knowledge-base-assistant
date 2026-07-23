259744d3ec050cf5b59d7fccded360af55699488
mouni <naremmounika@gmail.com>
2026-07-23 08:38:25 +0530

    Fix API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-knowledge-base-assistant-4e42.onrender.com",
 });
 
 api.interceptors.request.use(
