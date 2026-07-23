75c5cc5a7e3d1639eebe90f1fd8d1a7bea1f8e73
mouni <naremmounika@gmail.com>
2026-07-23 08:45:49 +0530

    fix api base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 
-
-const API = axios.create({
-    baseURL:"https://ai-knowledge-base-assistant-4e42.onrender.com"
+const api = axios.create({
+  baseURL: "https://ai-knowledge-base-assistant-4e42.onrender.com/api",
 });
 
-
-API.interceptors.request.use((config)=>{
-
+api.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem("token");
 
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-});
-
-
-export default API;
+export default api;
